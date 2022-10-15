877d2230c5271e2d971b3f6fe0d3fe0419f27bfc
Sandeshkgowda <sandeshkgowda.xworkz@gmail.com>
2022-10-15 20:43:12 +0530

    java

--- BikeShowRoomTester.java
+++
@@ -1,9 +0,0 @@
-class BikeShowRoomTester{
-public static void main(String agr[]){
-String bikes[] = {"ktm","plusar 220","splender","dio","active","passion pro","horrent","pleasure","xstream","tvs xl"};
-BikeShowRoom.getBike(bikes);
-}
-
-
-
-}
--- Sub1.java
+++
@@ -1,8 +0,0 @@
-class Sub1{
-public static void sub(int q,int w){
-System.out.println(q-w);
-}
-public static void sub(int q,int w,int t){
-System.out.println(q-w-t);
-}
-}
